512bab8c059c54c45d6452dc9424e7f9795e7d8f
buccijr <antoniobuccijr@gmail.com>
2025-08-16 15:09:50 -0400

    update 1 8/16/25

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"1425e5e9ec5eeb4f225c401d8db69b860e0fde
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "625121757"
+    serviceWorkerVersion: "2855642649"
   }
 });
